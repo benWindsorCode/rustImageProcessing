3a7ceecd2dac91d4232b81d64b40dc2ca0e9d7f4
benWindsorCode <benjaminwindsor@hotmail.co.uk>
2022-06-05 15:17:27 +0100

    Improve docs

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,18 @@ fn main() {
     edges.save("./images/edgeDetected.png");
 }
 
+
+/// Edge detection by the following process:
+///     1) increase image contrast
+///     2) Sharpen image
+///     3) Compute x and y gradients
+///     4) Add x and y gradients
+///
+/// # Arguments
+///
+/// * `input`: ImageBuffer to edge detect
+///
+/// returns: ImageBuffer
 fn edge_detect(input: &ImageBuffer) -> ImageBuffer {
     let cleaned = contrast(input, 2.);
     let sharpened = sharpen(&cleaned, 10.);
@@ -42,6 +54,17 @@ fn y_grad(input: &ImageBuffer) -> ImageBuffer {
     apply_matrix(input, matrix)
 }
 
+/// Perform sharpening of an image by:
+///     1) Perform a bilinear blur filter
+///     2) Subtract the Image - Bilinear Output, to get the 'detail' of the image
+///     3) Add specified multiple of detail back to original image
+///
+/// # Arguments
+///
+/// * `input`: ImageBuffer to sharpen
+/// * `value`: Multiple of detail to add on
+///
+/// returns: ImageBuffer
 fn sharpen(input: &ImageBuffer, value: f32) -> ImageBuffer {
     let filtered = bilinear_filter(input);
 
@@ -73,6 +96,20 @@ fn gaussian_blur(input: &ImageBuffer) -> ImageBuffer {
     apply_matrix(input, gaussian)
 }
 
+/// Apply a matrix to the input image, pixel by pixel where:
+///     g(i, j) = sum f(i + k, j + l)h(k, l)
+/// Where:
+///     g is new pixel of image
+///     f is current pixel of image
+///     h is the matrix
+///     k, l range over the dimensions of the matrix
+///
+/// # Arguments
+///
+/// * `input`: ImageBuffer to apply matrix to
+/// * `matrix`: Matrix to apply
+///
+/// returns: ImageBuffer
 fn apply_matrix(input: &ImageBuffer, matrix: Array2<f32>) -> ImageBuffer {
     let (input_x, input_y) = input.dimensions();
     let mut output: ImageBuffer = image::ImageBuffer::new(input_x, input_y);
@@ -97,8 +134,6 @@ fn apply_matrix(input: &ImageBuffer, matrix: Array2<f32>) -> ImageBuffer {
                 let input_curr = input.get_pixel(x_curr as u32, y_curr as u32);
                 let matrix_curr = matrix[[i, j]];
 
-                // println!("Image: ({}, {}), Matrix: ({}, {}). Image: {:?}, Matrix: {}", x_curr, y_curr, i, j, &input_curr, matrix_curr);
-
                 let prod = vec![((input_curr[0] as f32) * matrix_curr) as i32, ((input_curr[1] as f32) * matrix_curr) as i32, ((input_curr[2] as f32) * matrix_curr) as i32];
 
                 pixels_to_sum.push(prod);
@@ -106,7 +141,6 @@ fn apply_matrix(input: &ImageBuffer, matrix: Array2<f32>) -> ImageBuffer {
         }
 
         let mut total: Vec<i32> = vec![0, 0, 0];
-        // println!("Sum of: {:?}", &pixels_to_sum);
 
         for pixel_to_sum in pixels_to_sum {
             total = vec![total[0] + pixel_to_sum[0], total[1] + pixel_to_sum[1], total[2] + pixel_to_sum[2]];
@@ -133,7 +167,6 @@ fn median_filter(input: &ImageBuffer, window: i32) -> ImageBuffer {
 
         for i in (-1*window)..(window+1) {
             for j in (-1*window)..(window+1) {
-                // println!("i: {}, j: {}", i, j);
                 let x_curr = (x as i32) + i;
                 let y_curr = (y as i32) + j;
 
@@ -205,6 +238,15 @@ fn image_add(input_1: &ImageBuffer, input_2: &ImageBuffer) -> ImageBuffer {
     output
 }
 
+/// For each pixel, p, of an image, adjust brightness by output of:
+///     p + value
+///
+/// # Arguments
+///
+/// * `input`: Image buffer
+/// * `value`: Brightness addition value
+///
+/// returns: ImageBuffer
 fn adjust_brightness(input: &ImageBuffer, value: i32) -> ImageBuffer {
     let (input_x, input_y) = input.dimensions();
 
@@ -217,6 +259,15 @@ fn adjust_brightness(input: &ImageBuffer, value: i32) -> ImageBuffer {
     output
 }
 
+/// For each pixel, p, of an image, adjust contrast by output of:
+///     p * value
+///
+/// # Arguments
+///
+/// * `input`: Image buffer
+/// * `value`: Contrast scale value
+///
+/// returns: ImageBuffer
 fn adjust_contrast(input: &ImageBuffer, value: f32) -> ImageBuffer {
     let (input_x, input_y) = input.dimensions();
 
@@ -230,21 +281,55 @@ fn adjust_contrast(input: &ImageBuffer, value: f32) -> ImageBuffer {
 }
 
 fn pixel_sub(pixel_1: image::Rgba<u8>, pixel_2: image::Rgba<u8>) -> image::Rgba<u8> {
-    image::Rgba([safe_add(pixel_1[0], -1 * (pixel_2[0] as i32)), safe_add(pixel_1[1], -1 * (pixel_2[1] as i32)), safe_add(pixel_1[2], -1 * (pixel_2[2] as i32)), pixel_1[3]])
+    image::Rgba([
+        safe_add(pixel_1[0], -1 * (pixel_2[0] as i32)),
+        safe_add(pixel_1[1], -1 * (pixel_2[1] as i32)),
+        safe_add(pixel_1[2], -1 * (pixel_2[2] as i32)),
+        pixel_1[3]
+    ])
 }
 
 fn pixel_add(pixel_1: image::Rgba<u8>, pixel_2: image::Rgba<u8>) -> image::Rgba<u8> {
-    image::Rgba([safe_add(pixel_1[0], pixel_2[0] as i32), safe_add(pixel_1[1], pixel_2[1] as i32), safe_add(pixel_1[2], pixel_2[2] as i32), pixel_1[3]])
+    image::Rgba([
+        safe_add(pixel_1[0], pixel_2[0] as i32),
+        safe_add(pixel_1[1], pixel_2[1] as i32),
+        safe_add(pixel_1[2], pixel_2[2] as i32),
+        pixel_1[3]
+    ])
 }
 
+/// Shift a pixels r,g,b values by a constant value (positive or negative)
+///
+/// # Arguments
+///
+/// * `pixel`: Single  pixel of an image
+/// * `value`: Positive or negative value to shift the pixel by
+///
+/// returns: rgba pixel
 fn pixel_shift(pixel: image::Rgba<u8>, value: i32) -> image::Rgba<u8> {
     image::Rgba([safe_add(pixel[0], value), safe_add(pixel[1], value), safe_add(pixel[2], value), pixel[3]])
 }
 
+/// Scales (multiplies) a pixels r,g,b values by a constant value
+///
+/// # Arguments
+///
+/// * `pixel`: Single  pixel of an image
+/// * `value`: Scale factor of the pixel
+///
+/// returns: rgba pixel
 fn pixel_scale(pixel: image::Rgba<u8>, value: f32) -> image::Rgba<u8> {
     image::Rgba([safe_mult(pixel[0], value), safe_mult(pixel[1], value), safe_mult(pixel[2], value), pixel[3]])
 }
 
+/// Given a u8 and an integer, perform addition in the i32 space but then clamp back to a u8
+///
+/// # Arguments
+///
+/// * `a`: u8 value
+/// * `b`: i32 value
+///
+/// returns: u8
 fn safe_add(a: u8, b: i32) -> u8 {
     let c = (a as i32) + b;
     let scaled = cmp::min(255, cmp::max(0, c));
@@ -252,6 +337,14 @@ fn safe_add(a: u8, b: i32) -> u8 {
     scaled as u8
 }
 
+/// Given a u8 and a float, multiply as floats, round to i32 then clamb pack to to a u8
+///
+/// # Arguments
+///
+/// * `a`: u8 value
+/// * `b`: f32 value
+///
+/// returns: u8
 fn safe_mult(a: u8, b: f32) -> u8 {
     let c = ((a as f32) * b) as i32;
     let scaled = cmp::min(255, cmp::max(0, c));
